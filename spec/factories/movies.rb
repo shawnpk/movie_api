fd64d3ad7c86bf41cfa8475dd71ab5dcd68ed415
Shawn Kearney <shawnpk29@me.com>
2020-07-17 11:19:58 -0600

    Update movie factory

--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :movie do
-    title { "MyString" }
-    plot { "MyString" }
-    release_date { "2020-07-17" }
+    title { Faker::Movie.title }
+    plot { Faker::Movie.quote }
+    release_date { Faker::Date.between(from: 70.years.ago, to: Date.today) }
   end
 end
