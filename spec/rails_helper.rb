ac42eadb50b4a0cbb5dec779cc7934bd1c119bc8
Shawn Kearney <shawnpk29@me.com>
2020-07-17 10:49:43 -0600

    Fix finger-check

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,7 +34,7 @@ end
 # Shoulda matchers configuration
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
-    with.test_frameword :rspec
+    with.test_framework :rspec
     with.library :rails
   end
 end
